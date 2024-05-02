89a5ef403f46cf5476ac62afa9bf647d0aaefc43
ZxStim <thang.victor.luong@gmail.com>
2024-05-02 13:52:55 +0700

    added contributors

--- app/contributors/data.tsx
+++ app/contributors/data.tsx
@@ -4,19 +4,30 @@ export const coreContributors = [
     name: "ZxStim",
     role: "Core",
     twitter: "https://twitter.com/zxstim",
+    github: "https://github.com/zxstim"
   },
   {
     id: 2,
     name: "Paulus",
     role: "Core",
     twitter: "https://twitter.com/pauluspham0701",
+    github: "https://github.com/phatpham0701"
   },
   {
     id: 3,
     name: "Dylan",
     role: "Core",
     twitter: "https://twitter.com/0xdyln",
+    github: "https://github.com/dylanngph"
   }
 ]
 
-export const contributors = []
+export const contributors = [
+  {
+    id: 1,
+    name: "Anh Tuan",
+    role: "community",
+    twitter: "https://twitter.com/kay_x86",
+    github: "https://github.com/chauanhtuan185"
+  }
+]
--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { coreContributors } from '@/app/contributors/data'
+import { coreContributors, contributors } from '@/app/contributors/data'
 
 export default function Page() {
   return (
@@ -11,12 +11,21 @@ export default function Page() {
         {
           coreContributors.map((coreContributor) => (
             <>
-              <a key={coreContributor.id} className="text-md underline" href={coreContributor.twitter}>{coreContributor.name}</a>
+              <a key={coreContributor.id} className="text-md underline w-fit" href={coreContributor.github} target='_blank'>{coreContributor.name}</a>
             </>
           ))
         }
       </div>
       <h2 className="text-xl font-semibold">community</h2>
+      <div className="flex flex-col gap-2">
+        {
+          contributors.map((coreContributor) => (
+            <>
+              <a key={coreContributor.id} className="text-md underline w-fit" href={coreContributor.github} target='_blank'>{coreContributor.name}</a>
+            </>
+          ))
+        }
+      </div>
     </div>
   );
 }
