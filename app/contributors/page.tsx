9bdc70a7713309f990683ee16a54622bbac602af
ZxStim <thang.victor.luong@gmail.com>
2024-05-05 16:54:51 +0700

    edit contributors styling

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
       <h1 className="text-3xl md:text-5xl font-bold">Contributors</h1>
       <Link className="underline text-blue-500" href="/">return home</Link>
       <h2 className="text-xl font-semibold">core</h2>
-      <div className="flex md:flex-row md:flex-wrap flex-col gap-2 md:gap-8">
+      <div className="flex flex-row flex-wrap gap-4 md:gap-8">
         {
           coreContributors.map((coreContributor) => (
-            <div key={coreContributor.id} className="flex flex-row gap-4 border-2 border-primary p-2 rounded-md">
+            <div key={coreContributor.id} className="flex flex-row gap-4 border-2 border-primary p-2 rounded-md w-fit">
               <a className="text-md underline w-fit" href={coreContributor.github} target='_blank'>{coreContributor.name}</a>
               <a className="text-md underline w-fit" href={coreContributor.github} target='_blank'>𝕏</a>
             </div>
@@ -18,10 +18,10 @@ export default function Page() {
         }
       </div>
       <h2 className="text-xl font-semibold">community</h2>
-      <div className="flex md:flex-row md:flex-wrap flex-col gap-2 md:gap-8">
+      <div className="flex flex-row flex-wrap gap-4 md:gap-8">
         {
           contributors.map((contributor) => (
-            <div key={contributor.id} className="flex flex-row gap-4 border-2 border-primary p-2 rounded-md">
+            <div key={contributor.id} className="flex flex-row gap-4 border-2 border-primary p-2 rounded-md w-fit">
               <a className="text-md underline w-fit" href={contributor.github} target='_blank'>{contributor.name}</a>
               <a className="text-md underline w-fit" href={contributor.github} target='_blank'>𝕏</a>
             </div>
