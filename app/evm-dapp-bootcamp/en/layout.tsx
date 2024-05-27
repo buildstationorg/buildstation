adb6db3b3e44dc9769e786919830b75ef621fe23
ZxStim <thang.victor.luong@gmail.com>
2024-05-27 23:01:56 +0700

    fixed content in vi bootcamp

--- app/evm-dapp-bootcamp/en/layout.tsx
+++ app/evm-dapp-bootcamp/en/layout.tsx
@@ -6,16 +6,18 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { dappBootcampsCategoryNavData, dappBootcampsItemNavData } from './data'
-import SidebarNav from '@/components/sidebar-nav'
+import { dappBootcampsCategoryNavData, dappBootcampsItemNavData } from "./data";
+import SidebarNav from "@/components/sidebar-nav";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-
   // Create any shared layout or styles here
   return (
     <div className="grid grid-cols-1 md:grid-cols-[320px_minmax(768px,_1fr)] text-left gap-8 justify-center">
-      <SidebarNav categories={dappBootcampsCategoryNavData} items={dappBootcampsItemNavData} />
-      <div className='flex flex-col gap-8 w-full'>
+      <SidebarNav
+        categories={dappBootcampsCategoryNavData}
+        items={dappBootcampsItemNavData}
+      />
+      <div className="flex flex-col gap-8 w-full">
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
@@ -33,12 +35,12 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-              <BreadcrumbPage>vi</BreadcrumbPage>
+              <BreadcrumbPage>en</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
-        </Breadcrumb>        
+        </Breadcrumb>
         {children}
       </div>
     </div>
-  )
+  );
 }
