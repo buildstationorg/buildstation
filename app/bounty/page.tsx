ed7fd2c7c6f513150d730e7688c904e60e0a8922
ZxStim <thang.victor.luong@gmail.com>
2024-07-15 13:28:26 +0700

    added bounty alert

--- app/bounty/page.tsx
+++ app/bounty/page.tsx
@@ -10,6 +10,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { AlertTriangle } from "lucide-react";
 
 export default function Page() {
   return (
@@ -26,6 +27,10 @@ export default function Page() {
         </BreadcrumbList>
       </Breadcrumb>
       <h1 className="text-3xl md:text-5xl font-bold">bounty</h1>
+      <div className="flex flex-row gap-2 items-center bg-yellow-400 py-2 px-4">
+        <AlertTriangle className="w-6 h-6" />
+        <p>we are working on our bounty system, please check back later!</p>
+      </div>
       <DataTable columns={columns} data={data} />
     </div>
   );
