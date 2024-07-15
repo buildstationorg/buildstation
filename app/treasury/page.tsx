ea0b14537f4f54486af0e704b53dca97e60a36ca
ZxStim <thang.victor.luong@gmail.com>
2024-07-15 16:12:34 +0700

    edit treasury to include donation

--- app/treasury/page.tsx
+++ app/treasury/page.tsx
@@ -9,7 +9,7 @@ import {
 import { arbitrumPublicClient, klaytnPublicClient } from "./client";
 import { formatEther } from "viem";
 import { ExternalLink } from "lucide-react";
-import A from "@/components/mdx/a";
+import { Info } from 'lucide-react';
 
 async function getMultichainBalances() {
   const arbitrumBalance = await arbitrumPublicClient.getBalance({
@@ -42,6 +42,18 @@ export default async function Page() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         treasury
       </h1>
+      <div className="flex flex-col gap-2 bg-blue-300 py-2 px-4">
+        <h2 className="flex flex-row gap-2 items-center text-lg font-semibold">
+          <Info className="w-4 h-4" />Donate
+        </h2>
+        <p className="leading-7 text-sm">
+          if you would like to donate to the treasury, please send your donation to
+        </p>
+        <code className="">
+          0x439aa01146DEB050881a254c7490c7f466e4D88d
+        </code>
+        <p className="leading-7 text-sm">beside mainnet assets, we welcome testnet native tokens as well</p>
+      </div>
       <div className="flex flex-col gap-2">
         <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight">
           Arbitrum
@@ -70,6 +82,8 @@ export default async function Page() {
           <ExternalLink className="w-4 h-4 ml-2" />
         </a>
       </div>
+
+
     </div>
   );
 }
