391cd7593e46643affd576b7a18f1219c96760ae
ZxStim <thang.victor.luong@gmail.com>
2024-10-18 20:22:56 +0700

    updated layouts

--- app/awesome-buildstation/layout.tsx
+++ app/awesome-buildstation/layout.tsx
@@ -1,27 +1,8 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
     <div className="flex flex-col text-left gap-4 max-w-3xl w-full">
-      <Breadcrumb className="mb-8">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">home</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>awesome buildstation</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
       {children}
     </div>
   );
