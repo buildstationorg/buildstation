e9027b2c18e54971a2ababb3cafd4ef37ebd44bc
ZxStim <thang.victor.luong@gmail.com>
2024-11-01 11:19:34 +0700

    added scroll treasury

--- app/treasury/client.ts
+++ app/treasury/client.ts
@@ -1,7 +1,12 @@
 import { http, createPublicClient } from 'viem'
-import { arbitrum } from 'viem/chains'
+import { arbitrum, scroll } from 'viem/chains'
  
 export const arbitrumPublicClient = createPublicClient({
   chain: arbitrum,
+  transport: http(),
+})
+
+export const scrollPublicClient = createPublicClient({
+  chain: scroll,
   transport: http(),
 })
--- app/treasury/page.tsx
+++ app/treasury/page.tsx
@@ -6,23 +6,31 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { arbitrumPublicClient } from "./client";
-import { formatEther } from "viem";
+import { arbitrumPublicClient, scrollPublicClient } from "./client";
+import { formatEther, erc20Abi } from "viem";
 import { ExternalLink } from "lucide-react";
 import { Info } from 'lucide-react';
 import { CopyButton } from '@/components/copy-button';
 
 async function getMultichainBalances() {
-  const arbitrumBalance = await arbitrumPublicClient.getBalance({
-    address: "0x439aa01146DEB050881a254c7490c7f466e4D88d",
-  });
+  const [arbitrumBalance, scrollBalance] = await Promise.all([
+    arbitrumPublicClient.getBalance({
+      address: "0xc14c279967cd712FFEEDcFD4e56C845216b5042D",
+    }),
+    scrollPublicClient.readContract({
+      address: "0xd29687c813D741E2F938F4aC377128810E217b1b",
+      abi: erc20Abi,
+      functionName: "balanceOf",
+      args: ["0xc14c279967cd712FFEEDcFD4e56C845216b5042D"],
+    }),
+  ]);
 
-  return { arbitrumBalance };
+  return { arbitrumBalance, scrollBalance };
 }
 export const fetchCache = "force-no-store";
 
 export default async function Page() {
-  const { arbitrumBalance } = await getMultichainBalances();
+  const { arbitrumBalance, scrollBalance } = await getMultichainBalances();
 
   return (
     <div className="flex flex-col gap-8 w-full">
@@ -49,13 +57,13 @@ export default async function Page() {
         </p>
         <div className="flex flex-row">
           <code className="bg-blue-600 py-2 px-4 text-secondary overflow-x-auto">
-            0x439aa01146DEB050881a254c7490c7f466e4D88d
+            0xc14c279967cd712FFEEDcFD4e56C845216b5042D
           </code>
           <div className="flex flex-row text-secondary px-2 py-2 text-center items-center bg-blue-600">
-            <CopyButton text="0x439aa01146DEB050881a254c7490c7f466e4D88d" />
+            <CopyButton text="0xc14c279967cd712FFEEDcFD4e56C845216b5042D" />
           </div>
         </div>
-        <p className="leading-7 text-sm">beside mainnet assets, we welcome testnet native tokens as well</p>
+        <p className="leading-7 text-sm">on Arbitrum, Base or Scroll.</p>
       </div>
       <div className="flex flex-col gap-2">
         <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight">
@@ -63,11 +71,25 @@ export default async function Page() {
         </h2>
         <p>{formatEther(arbitrumBalance)} ETH</p>
         <a
-          href="https://arbiscan.io/address/0x439aa01146DEB050881a254c7490c7f466e4D88d"
+          href="https://arbiscan.io/address/0xc14c279967cd712FFEEDcFD4e56C845216b5042D"
           target="_blank"
           className="flex flex-row items-center underline underline-offset-2 text-blue-500 w-fit"
         >
           Arbiscan
+          <ExternalLink className="w-4 h-4 ml-2" />
+        </a>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight">
+          Scroll
+        </h2>
+        <p>{formatEther(scrollBalance)} SCR</p>
+        <a
+          href="https://scrollscan/address/0xc14c279967cd712FFEEDcFD4e56C845216b5042D"
+          target="_blank"
+          className="flex flex-row items-center underline underline-offset-2 text-blue-500 w-fit"
+        >
+          Scrollscan
           <ExternalLink className="w-4 h-4 ml-2" />
         </a>
       </div>
