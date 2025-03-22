942f605a29a0a66036d778850060b67858153e8e
ZxStim <thang.victor.luong@gmail.com>
2025-03-22 15:05:56 +0700

    fix: unused imports

--- components/dashboard-content.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardContent() {
-  return (
-    <div className="flex flex-row items-center mt-8">
-      <p>stay tuned! we are building this at the moment</p>
-    </div>
-  );
-}
--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,7 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import RequestFaucet from "@/components/request-faucet";
-
 export default function Header() {
   return (
     <div className="flex flex-col gap-4 md:flex lg:flex-row lg:justify-between items-center border-b-2 pb-6 md:py-6 border-primary md:px-4">
       <h1 className="text-xl lg:text-4xl font-extrabold">buildstation<span className="text-xl lg:text-4xl font-medium bg-primary rounded-md text-secondary px-2 py-1 ml-2">HQ</span></h1>
-      <ConnectButton />
     </div>
   );
 }
