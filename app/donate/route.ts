17abd7aa65a8946c0a911069ce5054e82f9b2a7d
ZxStim <thang.victor.luong@gmail.com>
2025-05-13 23:12:31 +0700

    feat: added donate curl

---
+++ app/donate/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return new NextResponse("send any funds to buildstation.eth or 0xd06c478DbfE22c014EA0E76A0BB216f346e2EbDB.");
+}
